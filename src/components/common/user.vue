22908296fdaef55265e647ea11050a3210466178
Arif Ipek <arif@ipek.dev>
2023-05-29 19:12:01 +0300

    fixes on edit component and remove `socket/index.js`

---
+++ src/components/common/user.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-if="item && Object.keys(item).length > 0"
+    v-tooltip="item.full_name"
+    class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 cursor-pointer">
+    <span class="font-medium text-gray-600 dark:text-gray-300">{{ initials }}</span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'user',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    initials() {
+      const firstNameInitial = this.item.first_name[0];
+      const lastNameInitial = this.item.last_name[0];
+
+      return `${firstNameInitial}${lastNameInitial}`;
+    },
+  },
+};
+</script>
--- src/pages/documents/edit.vue
+++ src/pages/documents/edit.vue
@@ -1,7 +1,16 @@
 <template>
   <div class="max-w-6xl h-screen mx-auto">
     <loading v-if="isLoading"/>
-    <div class="edit-area" v-else>
+    <div class="editor-area" v-else>
+      <div v-if="viewers && viewers.length > 0" class="editor-header flex items-center justify-end">
+        <div class="flex mb-5 -space-x-4">
+          <user
+            v-for="(viewer, index) in viewers"
+            :key="`viewer-${index}-${viewer.email}-${viewer.id}`"
+            :item="viewer"
+          />
+        </div>
+      </div>
       <c-input v-model="form.title" class="mb-4"/>
       <textarea class="w-full px-5 py-4 rounded-md" style="height: calc(100vh - 50px);"
                 v-model="form.content"></textarea>
@@ -11,16 +20,18 @@
 
 <script>
 import { mapGetters } from 'vuex';
-import { setEditDocumentId, removeEditDocumentId } from '@/utils/cookie-helper';
-import { socket, state } from '@/socket';
+// import { setEditDocumentId, removeEditDocumentId } from '@/utils/cookie-helper';
+// import { socket, state } from '@/socket';
 import loading from '@/components/common/loading';
 import cInput from '@/components/common/c-input';
+import user from '@/components/common/user';
 
 export default {
   name: 'DocumentEdit',
   components: {
     loading,
     cInput,
+    user,
   },
   computed: {
     ...mapGetters('document', [
@@ -28,27 +39,70 @@ export default {
     ]),
     ...mapGetters('auth', [
       'getUser',
+      'getToken',
     ]),
-    status() {
-      return state.status;
-    },
-    viewers() {
-      return state.viewers;
-    },
-    documentState() {
-      return state.document;
-    },
   },
   data() {
     return {
       isLoading: false,
       form: {
-        title: state.document.title,
-        content: state.document.content,
+        title: '',
+        content: '',
       },
+      status: WebSocket.CONNECTING,
+      socket: null,
+      viewers: [],
     };
   },
   methods: {
+    connect() {
+      const url = `ws://localhost:8001/ws/doc/${this.$route.params.documentId}/edit/?token=${this.getToken.access_token}`;
+
+      this.status = WebSocket.CONNECTING;
+      this.socket = new WebSocket(url);
+
+      this.socket.onopen = () => {
+        console.log('Connected 🎉');
+        this.status = WebSocket.OPEN;
+      };
+
+      this.socket.onerror = (error) => {
+        console.log('An error occurred while connection establishing ⛔️', error);
+        this.status = WebSocket.CLOSED;
+      };
+
+      this.socket.onclose = () => {
+        console.log('Disconnected ✅');
+        this.status = WebSocket.CLOSED;
+
+        // Try to reconnect.
+        setTimeout(() => {
+          this.connect();
+        }, 3000);
+      };
+
+      this.socket.addEventListener('message', (event) => {
+        const message = JSON.parse(event.data);
+        const eventData = message.data;
+
+        if (message.type === 'document_updated') {
+          console.log('document_updated event received');
+          const { title, content } = eventData;
+
+          this.form.title = title;
+          this.form.content = content;
+        } else if (message.type === 'viewer_join') {
+          console.log('viewer_join event received');
+          this.viewers.push(eventData);
+        } else if (message.type === 'viewer_leave') {
+          console.log('viewer_leave event received');
+          this.viewers = this.viewers.filter((item) => item.id !== eventData.id);
+        }
+      });
+    },
+    connectSocket() {
+      this.connect();
+    },
     fetchDocument() {
       this.isLoading = true;
       this.$store.dispatch('document/getDocument', {
@@ -62,57 +116,48 @@ export default {
         this.isLoading = false;
       });
     },
-    leaveAndCloseConnection() {
-      if (this.status === WebSocket.OPEN) {
+  },
+  created() {
+    this.fetchDocument();
+    this.connectSocket();
+  },
+  watch: {
+    status: function handle(value) {
+      if (value === WebSocket.OPEN) {
+        const data = {
+          type: 'viewer_join',
+          data: this.getUser,
+        };
+        this.socket.send(JSON.stringify(data));
+      } if (value === WebSocket.CLOSING || value === WebSocket.CLOSED) {
         const data = {
           type: 'viewer_leave',
           data: this.getUser,
         };
-        socket.send(JSON.stringify(data));
+        this.socket.send(JSON.stringify(data));
       }
     },
-    closeSocketConnection() {
-      socket.close();
-    },
-  },
-  created() {
-    setEditDocumentId(this.$route.params.documentId);
-    this.fetchDocument();
-  },
-  mounted() {
-    if (this.status === WebSocket.OPEN) {
-      const data = {
-        type: 'viewer_join',
-        data: this.getUser,
-      };
-      socket.send(JSON.stringify(data));
-    }
-  },
-  beforeUnmount() {
-    removeEditDocumentId();
-    this.leaveAndCloseConnection();
-  },
-  unmounted() {
-    this.closeSocketConnection();
-  },
-  watch: {
     'form.title': function handle() {
-      if (this.status === WebSocket.OPEN) {
-        const data = {
-          type: 'document_updated',
-          data: this.form,
-        };
-        socket.send(JSON.stringify(data));
-      }
+      setTimeout(() => {
+        if (this.status === WebSocket.OPEN) {
+          const data = {
+            type: 'document_updated',
+            data: this.form,
+          };
+          this.socket.send(JSON.stringify(data));
+        }
+      }, 200);
     },
     'form.content': function handle() {
-      if (this.status === WebSocket.OPEN) {
-        const data = {
-          type: 'document_updated',
-          data: this.form,
-        };
-        socket.send(JSON.stringify(data));
-      }
+      setTimeout(() => {
+        if (this.status === WebSocket.OPEN) {
+          const data = {
+            type: 'document_updated',
+            data: this.form,
+          };
+          this.socket.send(JSON.stringify(data));
+        }
+      }, 200);
     },
   },
 };
